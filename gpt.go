b433e65b4b8ddc5c4c8a253ec6c3a7f8072cb132
Impervguin <leon53000@yandex.ru>
2023-12-02 00:58:32 +0300

    Done simple gpt client funcs

---
+++ gpt.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+
+var GPT_ADDRESS string = "http://127.0.0.1:8080/message"
+var GPT_MODEL   string = "gpt-3.5-turbo"
+var MAX_TOKEN   string = "100"
+
+func Send_gpt_message (messages *[]map[string]string) error {
+	fmt.Println("1")
+	request := map[string][]map[string]string{}
+	request["info"] = []map[string]string{{"model" : GPT_MODEL, "max_token": MAX_TOKEN}}
+	request["messages"] = *messages
+
+	json_data, err := json.Marshal(request)
+
+    if err != nil {
+        return err
+    }
+
+	response, err := http.Post(GPT_ADDRESS, "application/json", bytes.NewBuffer(json_data))
+
+	if err != nil {
+        return err
+    }
+	defer response.Body.Close()
+
+	var data map[string]string
+
+    body, err := io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	st := []byte(body)
+	json.Unmarshal(st, &data);
+	// fmt.Println(st)
+	// fmt.Println(data)
+	*messages = append(*messages, data)
+	return nil
+}
+
+func Get_gpt_message(messages *[]map[string]string) (string, error) {
+	if len(*messages) == 0 {
+		return "", fmt.Errorf("Нет сообщений")
+	}
+
+	if (*messages)[len(*messages) - 1]["role"] != "assistant" {
+		return "", fmt.Errorf("Последнее сообщение не бота.")
+	}
+
+	return (*messages)[len(*messages) - 1]["content"], nil	
+}
